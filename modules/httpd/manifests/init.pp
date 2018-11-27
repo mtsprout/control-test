f90e528f2329b541aea10574dc16a947e2bdee80
Craig Sprout <craig.sprout@craig.sprout-C02MN7MHFD57>
2018-11-27 16:55:41 -0600

    Added motd and httpd modules.

---
+++ modules/httpd/manifests/init.pp
@@ -0,0 +1,27 @@
+class httpd {
+
+  package { 'httpd':
+    ensure => present,
+  }
+
+  file { '/var/www/html':
+     ensure => directory,
+     owner   => 'root',
+     group   => 'root',
+     mode    => '0755',
+		 require => Package['httpd'],
+  }
+
+  file { '/var/www/html/index.html':
+		ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    content => epp('httpd/index.html.epp'),
+		mode    => '0644',
+		require => File['/var/www/html'],
+	}
+
+	service { 'httpd':
+		ensure => running,
+	}
+}
