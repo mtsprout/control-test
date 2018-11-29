8fa651e2f8322d9a6adef04af028ac451a5dc0c4
Craig Sprout <craig@puppet.com>
2018-11-29 14:16:25 -0600

    Changed Apache and Nginx modules

--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -1,42 +1,6 @@
-class nginx {
-
-  package { 'nginx':
-    ensure  => present,
-    require => Package['httpd']
-  }
+class httpd {
 
   package { 'httpd':
     ensure => absent,
   }
-
-  file { '/usr/share/nginx/html':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'webdevs',
-    mode    => '2775',
-    require => Package['nginx'],
-  }
-
-  file { '/usr/share/nginx/html/index.html':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'webdevs',
-    content => epp('httpd/index.html.epp'),
-    mode    => '0664',
-    require => File['/usr/share/nginx/html'],
-  }
-
-  service { 'nginx':
-    ensure => running,
-  }
-
-  user { 'csprout':
-    ensure     => 'present',
-    uid        => '1001',
-    gid        => '1001',
-    groups     => 'webusers',
-    home       => '/home/csprout',
-    shell      => '/bin/bash'
-    managehome => 'true',
-    }
 }
