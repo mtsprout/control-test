e84feeb018dd177932ac1875557d495f44588bf9
Craig Sprout <craig@puppet.com>
2018-12-03 11:28:10 -0600

    added sudoers to nginx piece

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -25,7 +25,7 @@ class nginx {
   file { '/usr/share/nginx/html/index.html':
     ensure  => 'present',
     owner   => 'root',
-    group   => 'webusers',
+    groups   => 'webusers, sudoers',
     content => epp('httpd/index.html.epp'),
     mode    => '0664',
     require => File['/usr/share/nginx/html'],
